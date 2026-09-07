a93b4d3846671df4507ca4acea0cb85050bdc6e4
TrygveHJ <fgu.trygve@gmail.com>
2026-09-07 10:56:43 +0200

    add generation for hero content

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
 // slå dig løs her... 
 
+
+let heroElem = document.querySelector(".hero")
+let heroImg = document.createElement("img")
+heroImg.src = hero.image
+heroImg.classList.add("hero__img")
+
+heroElem.append(heroImg)
+
+let heroDiv = document.createElement("div")
+heroDiv.classList.add("hero__div")
+
+let heroHeadline = document.createElement("h1")
+heroHeadline.textContent = hero.headline
+heroDiv.append(heroHeadline)
+
+let heroPara = document.createElement("p")
+heroPara.textContent = hero.copy
+heroDiv.append(heroPara)
+
+let heroLink = document.createElement("a")
+heroLink.innerHTML = `<img src="${hero.icon}" />Explore`
+heroLink.setAttribute("href", "#")
+heroDiv.append(heroLink)
+
+heroElem.append(heroDiv)
+
+
+
+
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
